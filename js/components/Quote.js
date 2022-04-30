3a921a8589b9e52e4cdd50593e765ea3f50dd464
Jan <linksunten@posteo.de>
2022-04-30 18:26:34 +0200

    Update Quote.js

--- js/components/Quote.js
+++ js/components/Quote.js
@@ -1,5 +1,5 @@
-import React, { Component, Fragment } from 'react';
-import {Text} from 'react-native';
+import React, { Component } from 'react';
+import {StyleSheet, Text, View } from 'react-native';
 /*
 export default funkction Quote(props) {
     const { text, author } = props
@@ -17,22 +17,27 @@ export default class Quote extends Component {
         // const text = this.props.text;
         // const author = this.props.author;
         return(
-            <Fragment>
+            <View style={styles.container}>
             <Text style={styles.text}>{text}</Text>
             <Text style={styles.author}>&mdash; {author}</Text>
-            </Fragment>
+            </View>
         );
     }
 }
 
-const styles = {
+const styles = StyleSheet.create( {
+    container: {
+        borderWidth: 1,
+        borderColor: 'white',
+        padding: 30
+    },
     text: {
-        fontSize: 36,
+        fontSize: 20,
         fontStyle: 'italic',
-        color: 'white'
+        color: 'white',
     },
     author: {
-        fontSize: 20,
+        fontSize: 15,
         color: 'white'
     }
-};
+});
