1cf4de9098124e56761cb53fad464328b8626d21
Jan <linksunten@posteo.de>
2022-04-30 17:58:04 +0200

    Aufräumarbeiten
    Idee der Funktion kommentiert, da zustandslos

--- App.js
+++ App.js
@@ -1,18 +1,20 @@
 import React, { Component } from 'react';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, View } from 'react-native';
+
+import Quote from './js/components/Quote'
 
 const data = [
-{text: 'Hallo Welt', author: '-- Peter Pan'},
-{text: 'Der Mensch ist das Problem.', author: '-- Jan Jürgens'},
-{text: 'Probleme kann man niemals mit derselben Denkweise lösen, durch die sie entstanden sind.', author: '-- Albert Einstein'},
-{text: 'Zitat 1', author: '-- Autor'},
-{text: 'Zitat 2', author: '-- Autor'},
-{text: 'Zitat 3', author: '-- Autor'},
-{text: 'Zitat 4', author: '-- Autor'},
+{text: 'Hallo Welt', author: 'Peter Pan'},
+{text: 'Der Mensch ist das Problem.', author: 'Jan Jürgens'},
+{text: 'Probleme kann man niemals mit derselben Denkweise lösen, durch die sie entstanden sind.', author: 'Albert Einstein'},
+{text: 'Zitat 1', author: 'Autor'},
+{text: 'Zitat 2', author: 'Autor'},
+{text: 'Zitat 3', author: 'Autor'},
+{text: 'Zitat 4', author: 'Autor'},
 ];
 
 export default class App extends Component {
-  state = { index: 0 }; //initialer Zustand
+  state = { index: 0 };
 
   render() {
     let index = this.state.index;
@@ -24,12 +26,7 @@ export default class App extends Component {
     return (
 
       <View style={styles.container}>
-        <Text>
-          {quote.text}
-        </Text>
-        <Text>
-          {quote.author}
-        </Text>
+        <Quote text={quote.text} author={quote.author}/>
         <Button title="Nächstes Zitat" onPress={() => this.setState({ index: nextIndex })}/>
         <Button title="Voriges Zitat" onPress={() => this.setState({ index: prevIndex })}/>
       </View>
@@ -37,7 +34,7 @@ export default class App extends Component {
   }
 }
 
-// Styles für Aussehen und Layout
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
--- js/components/Quote.js
+++ js/components/Quote.js
@@ -1,10 +1,26 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import {Text} from 'react-native';
-
+/*
+export default funkction Quote(props) {
+    const { text, author } = props
+    return (
+        <Fragment>
+            <Text>{text}</Text>
+            <Text>-- {author}</Text>
+        </Fragment>
+    );
+}
+*/
 export default class Quote extends Component {
     render(){
+        const { text, author } = this.props; // destructuring
+        // const text = this.props.text;
+        // const author = this.props.author;
         return(
-            <Text>Zitat</Text>
+            <Fragment>
+            <Text>{text}</Text>
+            <Text>-- {author}</Text>
+            </Fragment>
         );
     }
 }
