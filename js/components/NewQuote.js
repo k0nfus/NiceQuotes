f952382f18c3193511f8b3e3174429fc3473087a
Jan <linksunten@posteo.de>
2022-04-30 21:27:44 +0200

    Update NewQuote.js

--- js/components/NewQuote.js
+++ js/components/NewQuote.js
@@ -2,6 +2,9 @@ import React, { Component } from "react";
 import { Button, Modal, StyleSheet, TextInput, View } from 'react-native';
 
 export default class NewQuote extends Component {
+    
+    state={ content: null, author: null };
+
     render() {
         // this.props.onSave
         return (
@@ -10,6 +13,18 @@ export default class NewQuote extends Component {
         animationType="slide"
         >
             <View style={styles.container}>
+             <TextInput style={[styles.input, { height: 150 }]} 
+            multiline={true} 
+            placeholder="Inhalt des Zitats" 
+            underlineColorAndroid="transparent" 
+            onChangeText={text => this.setState({ content: text })}
+             />   
+           
+             <TextInput style={styles.input}
+             placeholder="Autor:in des Zitats"
+             underlineColorAndroid="transparent"
+             onChangeText={text => this.setState({ author: text })}
+             />
             <Button title="Speichern" onPress={this.props.onSave} />
             </View>
         </Modal>
@@ -21,6 +36,17 @@ const styles = StyleSheet.create({
     container:{
     flex: 1,
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
+    paddingTop: 40,
+    },
+    input: {
+        borderWidth: 1,
+        borderColor: "deepskyblue",
+        borderRadius: 4,
+        fontSize: 20,
+        width: '80%',
+        marginBottom: 20,
+        padding: 10,
+        height: 50,
     }
 })
