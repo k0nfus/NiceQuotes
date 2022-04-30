82242b1b2d0620673b79a02d4bcb2b6d4a606298
Jan <linksunten@posteo.de>
2022-04-30 20:30:28 +0200

    Update NewQuote.js
    StyleSheet und Flexbox

--- js/components/NewQuote.js
+++ js/components/NewQuote.js
@@ -1,12 +1,26 @@
 import React, { Component } from "react";
-import { Button, TextInput } from 'react-native';
+import { Button, Modal, StyleSheet, TextInput, View } from 'react-native';
 
 export default class NewQuote extends Component {
     render() {
-        if(this.props.visible === false) {
-            return null;
-        }
         // this.props.onSave
-        return <Button title="Speichern" onPress={this.props.onSave} />;
+        return (
+        <Modal visible={this.props.visible}
+        onRequestClose={this.props.onSave}
+        animationType="slide"
+        >
+            <View style={styles.container}>
+            <Button title="Speichern" onPress={this.props.onSave} />
+            </View>
+        </Modal>
+        )
     }
 }
+
+const styles = StyleSheet.create({
+    container:{
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    }
+})
