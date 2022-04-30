08820bd99a24a909f98f43a478d32206e2ef6029
Jan <linksunten@posteo.de>
2022-04-30 17:40:29 +0200

    Neue Komponente
    Eigene Komponente

---
+++ js/components/Quote.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import {Text} from 'react-native';
+
+export default class Quote extends Component {
+    render(){
+        return(
+            <Text>Zitat</Text>
+        );
+    }
+}
